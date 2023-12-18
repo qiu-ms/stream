a70582b916f5c2bc6352034fabb1571c6e2465af
qiu-ms <qiumenseng@gmail.com>
2023-12-18 12:27:14 +0100

    Initial Commit

---
+++ stream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stream
+//
+//  Created by Qiu, Men Seng on 18.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stream/streamApp.swift
@@ -0,0 +1,17 @@
+//
+//  streamApp.swift
+//  stream
+//
+//  Created by Qiu, Men Seng on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct streamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
